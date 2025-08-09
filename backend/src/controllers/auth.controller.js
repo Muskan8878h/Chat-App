0b1cb84e5fd90a660f12a2bdf1ca6aa1b25abe77
Muskan8878h <muskan8878h@gmail.com>
2025-08-09 23:29:28 +0530

    created the data which is used during the sign is and handel errors also using try catch

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,9 +1,43 @@
-export const signup=(req,res)=>{
+import User from '../models/user.model.js'
+import bcrypt from 'bcryptjs'
+
+export const signup=async(req,res)=>{
     // res.send("signup router");
+
     const {fullName,password,email}=req.body;
+    
     try{
-        //hash password
+        //password
+        if(password.length < 8){
+            return re.status(400).json({message: "Password must be at least 8 characters"});
+        }
         
+        // email
+        const user=await User.findOne(email)
+        if(user) return res.status(400).json({message: "Email already exists"});
+
+        // hash password
+        const salt= await bcrypt.genSalt(10);
+        const hashedPassword=await bcrypt.hash(password,salt);
+
+        // new user data
+        const newUser=new User({
+            fullName: fullName,
+            email: email,
+            password: hashedPassword
+        })
+
+        // is there any error
+        if(newUser){
+            // generate JWT token here
+
+        }
+        else{
+            res.status(400).json({message: "Invalid user data"});
+        }
+
+
+
     }catch(error){
 
     }
