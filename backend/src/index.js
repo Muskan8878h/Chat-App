1217be799075af11cfe247a544a546e9c89d6af4
Muskan8878h <muskan8878h@gmail.com>
2025-08-09 17:58:26 +0530

    imort the route

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,11 @@
 // const express=require("express")  // used in common js
 import express from "express"
 
+import authRoutes from "./routes/auth.route"
+
 const app=express();
 
-app.use("/api/auth",AuthRoutes)
+app.use("/api/auth",authRoutes)
 
 app.listen(5001,()=>{
     console.log("serevr is running on port 5001")
