82ce0988dcd00612169505211416b99dbc8e7ed9
Muskan8878h <muskan8878h@gmail.com>
2025-08-10 00:19:11 +0530

    updated

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,15 +1,20 @@
+import { generateToken } from '../lib/utils.js';
 import User from '../models/user.model.js'
 import bcrypt from 'bcryptjs'
 
 export const signup=async(req,res)=>{
     // res.send("signup router");
 
-    const {fullName,password,email}=req.body;
+    const {fullName,email,password}=req.body;
     
     try{
         //password
+        if(!fullName || !email || !password){
+            return res.status(400).json({message: "All fields are required"});
+        }
+
         if(password.length < 8){
-            return re.status(400).json({message: "Password must be at least 8 characters"});
+            return res.status(400).json({message: "Password must be at least 8 characters"});
         }
         
         // email
@@ -30,16 +35,23 @@ export const signup=async(req,res)=>{
         // is there any error
         if(newUser){
             // generate JWT token here
+            generateToken(newUser._id,res)
+            await newUser.save()
 
+            // success
+            res.status(202).json({
+                _id:newUser._id,
+                fullName: fullName,
+                email: email,
+                profilePic: newUser.profilePic,
+            });
         }
         else{
             res.status(400).json({message: "Invalid user data"});
         }
-
-
-
     }catch(error){
-
+        console.log("Error in signup controller",error.message); 
+        res.status(500).json({message: "Internal Server Error"});
     }
 }
 
