a778f2ff409ed9856513b4e3d632540ab855eea3
Muskan8878h <muskan8878h@gmail.com>
2025-08-10 15:09:17 +0530

    updateprofile pic

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,8 @@
+import cloudinary from '../lib/cloudinary.js';
 import { generateToken } from '../lib/utils.js';
 import User from '../models/user.model.js'
 import bcrypt from 'bcryptjs'
+import cloudinary from '../lib/cloudinary.js';
 
 export const signup=async(req,res)=>{
     // res.send("signup router");
@@ -100,5 +102,21 @@ export const logout=(req,res)=>{
 };
 
 export const updateProfile=async(req,res)=>{
-    
+    try{
+        const {profilePic}=req.body;
+        const userId=req.user._id;
+
+        if(!profilePic){
+            return res.status(400).json({message: "Profile pic is required"})
+        }
+
+        const uploadResponse=await cloudinary.uploader.upload(profilePic);
+        const updateUser=await User.findByIdAndUpdate(userId,{profilePic:uploadResponse.secure_url});
+
+        res.status(200).json(updateUser);
+    }
+    catch(error){
+        console.log("error in updaete profile ", error);
+        return res.status(500).json({message: "Internal server error"});
+    }
 };
