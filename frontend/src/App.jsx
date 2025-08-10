84aacc5675e03ce46a4e3474c051ede2dd323326
Muskan8878h <muskan8878h@gmail.com>
2025-08-10 18:34:06 +0530

    updated that if user not login or sign in then user directly go to home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React,{useEffect} from 'react'
 import Navbar from './components/Navbar'
-import { Routes,Route } from 'react-router-dom'
+import { Routes,Route, Navigate } from 'react-router-dom'
 
 import HomePage from './pages/HomePage'
 import SignUpPage from './pages/SignUpPage'
@@ -20,7 +20,8 @@ const App = () => {
 
   console.log({authUser});
 
-  if(true) return(
+  // loader
+  if(isCheckingAuth && !authUser) return(
     <div className='flex items-center justify-center h-screen'>
       <Loader className="size-10 animate-spin" />
     </div>
@@ -31,11 +32,11 @@ const App = () => {
       <Navbar />
       
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
         <Route path="/settings" element={<SettingsPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" /> }/>
       </Routes>
 
     </div>
