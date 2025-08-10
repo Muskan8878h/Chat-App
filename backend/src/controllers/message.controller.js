ca84b0a47b07ad6bc1420e040b32976285ec79ea
Muskan8878h <muskan8878h@gmail.com>
2025-08-10 16:10:17 +0530

    controller for send mesage

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import User from '../models/user.model.js'
 import Message from '../models/message.model.js';
+import cloudinary from '../lib/cloudinary.js';
 
 export const getUsersForSidebar=async(req,res)=>{
     try{
@@ -33,3 +34,33 @@ export const getMessages=async(req,res)=>{
     }
 };
 
+export const sendMessages=async(req,res)=>{
+    try{
+        const {text,image}=req.body;
+        const {id:receiverId}=req.params;
+        const senderId=req.user._id;
+
+        let imageUrl;
+        if(image){
+            // upload base64 image to cloudinary
+            const uploadResponse=await cloudinary.uploader.upload(image);
+            imageUrl=uploadResponse.secure_url;
+        }
+
+        const newMessage=new Message({
+            senderId,
+            receiverId,
+            text,
+            image:imageUrl,
+        });
+
+        await newMessage.save();
+
+        // todo: realtime functionality goed here => socket.io
+        res.status(200).json(sendMessages);
+    }
+    catch(error){
+        console.log("Error in sendMessages controller :",error.message);
+        res.status(500).json({error: "Internal server error"});
+    }
+};
