b50b1396ce99d42dd48a9df2aaa50380a683e0bd
Muskan8878h <muskan8878h@gmail.com>
2025-08-10 18:07:17 +0530

    adding cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,7 @@ import authRoutes from "./routes/auth.route.js"
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser"
 import messageRoutes from "./routes/message.route.js"
+import cors from "cors"
 
 const app=express();
 
@@ -17,6 +18,10 @@ app.use(cookieParser());
 app.use("/api/auth",authRoutes)
 app.use("/api/message",messageRoutes)
 
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}))
 
 app.listen(PORT,()=>{
     console.log("serevr is running on port: "+PORT);
